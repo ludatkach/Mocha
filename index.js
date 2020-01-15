409526f295d7e948083cd59fe4a453fd8d220d60
Lyudmyla Tkachenko <luda_tkach@yahoo.com>
2020-01-15 00:43:03 -0800

    Minor changes

--- index.js
+++ index.js
@@ -1,73 +1,73 @@
 function sum( a, b ){
-    return a + b;
+  return a + b;
 }
 
 function mult( a, b ) {
-    return a * b;
+  return a * b;
 }
 function div( a, b ) {
-    if ( b === 0 )  {
-        return ('Division by zero');
-    }
-    return a / b;
+  if ( b === 0 )  {
+    return ('Division by zero');
+  }
+  return a / b;
 }
 
 function divNumbers(a, b) {
-    if (Number.isInteger(a / b)) {
-        return true;
-    }
-    return false;
+  if (Number.isInteger(a / b)) {
+    return true;
+  }
+  return false;
 }
 
 function isAdult ( number ) {
-    if( number < 0 )
-        return undefined;
-    if ( number > 20 ) {
-        return true;
-    }
-    return false;
+  if( number < 0 )
+    return undefined;
+  if ( number > 20 ) {
+    return true;
+  }
+  return false;
 }
 
 function sortArrayUp ( arrayNumbers ) {
-    if ( typeof(arrayNumbers) === 'undefined' ) {
-        return [];
-    }
-    return arrayNumbers.sort((a,b) => a - b);
+  if ( typeof(arrayNumbers) === 'undefined' ) {
+    return [];
+  }
+  return arrayNumbers.sort((a,b) => a - b);
 }
 
 function isPalindrome( string ) {
-    let rev = string.split('').reverse().join('').toLowerCase();
-    if ( string.toLowerCase() === rev ){
-        return true;
-    }
-    return false;
+  let rev = string.split('').reverse().join('').toLowerCase();
+  if ( string.toLowerCase() === rev ){
+    return true;
+  }
+  return false;
 }
 
 function isOddNumber(number) {
-    if ( number % 2 ) {
-        return true;
-    }
-    return false;
+  if ( number % 2 ) {
+    return true;
+  }
+  return false;
 }
 
 function isEvenNumber(number) {
-    if ( number % 2 === 0 ) {
-        return true;
-    }
-    return false;
+  if ( number % 2 === 0 ) {
+    return true;
+  }
+  return false;
 }
 
 function breakChocolate(n,m) {
-    if ( n < 1 || m < 1) {
-        return 0;
-    }
-    if ( n===1 || m === 1 ) {
-        return Math.max(m,n) - 1;
-    }
-    if ( !Number.isInteger(n) || !Number.isInteger(m) ) {
-        return null;
-    }
-    return (m -1) + m * ( n - 1);
+  if ( n < 1 || m < 1) {
+    return 0;
+  }
+  if ( n===1 || m === 1 ) {
+    return Math.max(m,n) - 1;
+  }
+  if ( !Number.isInteger(n) || !Number.isInteger(m) ) {
+    return null;
+  }
+  return (m -1) + m * ( n - 1);
 }
 
 module.exports = {sum, mult, div, divNumbers, isAdult, sortArrayUp, isPalindrome, isOddNumber, isEvenNumber, breakChocolate};
--- test/index.spec.js
+++ test/index.spec.js
@@ -3,154 +3,152 @@ const {expect} = require('chai');
 const {sum, mult, div, divNumbers, isAdult, sortArrayUp, isPalindrome, isOddNumber, isEvenNumber, breakChocolate} = require('../index.js');
 
 describe('function sum', () => {
-    it('should function sum equal 6', () => {
-        assert.equal(sum(2, 4),6);
-        expect(sum(2,4)).to.equal(6);
-    })
+  it('should function sum equal 6', () => {
+    assert.equal(sum(2, 4),6);
+    expect(sum(2,4)).to.equal(6);
+  });
 
-    it('should function sum not equals 0', () => {
-        assert.notEqual(sum(2, 4),0);
-        expect(sum(2,4)).to.not.equal(0);
-    })
-})
+  it('should function sum not equals 0', () => {
+    assert.notEqual(sum(2, 4),0);
+    expect(sum(2,4)).to.not.equal(0);
+  });
+});
 
 describe('function mult', () => {
-    it('should function mult works correctly', () => {
-        assert.equal(mult(2, 4), 8);
-        expect(mult(2, 4)).to.equal(8);
-    })
-    it('should function mult equal 0', () => {
-        assert.equal(mult(2, 0), 0);
-        expect(mult(2, 0)).to.equal(0);
-    })
-})
+  it('should function mult works correctly', () => {
+    assert.equal(mult(2, 4), 8);
+    expect(mult(2, 4)).to.equal(8);
+  });
+  it('should function mult equal 0', () => {
+    assert.equal(mult(2, 0), 0);
+    expect(mult(2, 0)).to.equal(0);
+  });
+});
 
 describe('function div', () => {
-    it('should function div works correctly', () => {
-        assert.equal(div(60, 2), 30);
-        expect(div(60, 2)).eq(30);
-    })
-    it('should function div equal \'Division by zero\'', () => {
-        assert.equal(div(20, 0), 'Division by zero');
-        expect(div(20, 0)).to.eq('Division by zero');
-    })
-})
+  it('should function div works correctly', () => {
+    assert.equal(div(60, 2), 30);
+    expect(div(60, 2)).eq(30);
+  });
+  it('should function div equal \'Division by zero\'', () => {
+    assert.equal(div(20, 0), 'Division by zero');
+    expect(div(20, 0)).to.eq('Division by zero');
+  });
+});
 
 describe('function divNumbers', () => {
-    it('should function divNumbers equal true', () => {
-        assert.equal(divNumbers(60, 2), true);
-        expect(divNumbers(60, 2)).to.be.true;
-    })
-    it('should function divNumbers equal false', () => {
-        assert.equal(divNumbers(20, null), false);
-        expect(divNumbers(20, 0)).to.be.false;
-    })
-})
+  it('should function divNumbers equal true', () => {
+    assert.equal(divNumbers(60, 2), true);
+    expect(divNumbers(60, 2)).to.be.true;
+  });
+  it('should function divNumbers equal false', () => {
+    assert.equal(divNumbers(20, null), false);
+    expect(divNumbers(20, 0)).to.be.false;
+  });
+});
 
 describe('function isAdult', () => {
-    it('should function isAdult equal 21', () => {
-        assert.equal(isAdult(21), true);
-        expect(isAdult(21)).to.be.true;
-    })
-    it('should function isAdult equal more than 21', () => {
-        assert.equal(isAdult(55), true);
-        expect(isAdult(55)).to.true;
-    })
-    it('should function isAdult not equal if less than 21', () => {
-        assert.notEqual(isAdult(2), true);
-        expect(isAdult(2)).to.not.true;
-    })
-    it('should function isAdult equal undefined', () => {
-        assert.equal(isAdult(-5), undefined);
-        expect(isAdult(-5)).undefined;
-    })
-})
+  it('should function isAdult equal 21', () => {
+    assert.equal(isAdult(21), true);
+    expect(isAdult(21)).to.be.true;
+  });
+  it('should function isAdult equal more than 21', () => {
+    assert.equal(isAdult(55), true);
+    expect(isAdult(55)).to.true;
+  });
+  it('should function isAdult not equal if less than 21', () => {
+    assert.notEqual(isAdult(2), true);
+    expect(isAdult(2)).to.not.true;
+  });
+  it('should function isAdult equal undefined', () => {
+    assert.equal(isAdult(-5), undefined);
+    expect(isAdult(-5)).undefined;
+  });
+});
 
 describe('function sortArrayUp', () => {
-    it('should function sortArrayUp works correctly', () => {
-        assert.deepEqual(sortArrayUp([2, 3, 4, 0, 1]), [0, 1, 2, 3, 4]);
-        expect(sortArrayUp([2, 3, 4, 0, 1])).to.deep.equal([0, 1, 2, 3, 4]);
-    })
-    it('should function sortArrayUp works correctly', () => {
-        assert.notEqual(sortArrayUp([1, 9, 125, 2, 289]), [1,2,4,9,125,289]);
-        expect(sortArrayUp([1, 9, 125, 2, 289])).to.not.equal([1,2,4,9,125,289]);
-    })
-    it('should function sortArrayUp equal empty array', () => {
-        assert.deepEqual(sortArrayUp(), []);
-        expect(sortArrayUp()).to.deep.equal([]);
-    })
-})
+  it('should function sortArrayUp works correctly', () => {
+    assert.deepEqual(sortArrayUp([2, 3, 4, 0, 1]), [0, 1, 2, 3, 4]);
+    expect(sortArrayUp([2, 3, 4, 0, 1])).to.deep.equal([0, 1, 2, 3, 4]);
+  });
+  it('should function sortArrayUp works correctly', () => {
+    assert.notEqual(sortArrayUp([1, 9, 125, 2, 289]), [1,2,4,9,125,289]);
+    expect(sortArrayUp([1, 9, 125, 2, 289])).to.not.equal([1,2,4,9,125,289]);
+  });
+  it('should function sortArrayUp equal empty array', () => {
+    assert.deepEqual(sortArrayUp(), []);
+    expect(sortArrayUp()).to.deep.equal([]);
+  });
+});
 
 describe('function isPalindrome', () => {
-    it('should function isPalindrome equal `ogo`', () => {
-        assert.equal(isPalindrome('ogo'), true);
-        expect(isPalindrome('ogo')).eq(true);
-    })
-    it('should function isPalindrome equal `gaga` be false', () => {
-        assert.equal(isPalindrome('gaga'), false);
-        expect(isPalindrome('gaga')).eq(false);
-    })
-    it('should function isPalindrome equal empty string', () => {
-        assert.equal(isPalindrome(''), true);
-        expect(isPalindrome('')).eq(true);
-    })
-})
+  it('should function isPalindrome equal `ogo`', () => {
+    assert.equal(isPalindrome('ogo'), true);
+    expect(isPalindrome('ogo')).eq(true);
+  });
+  it('should function isPalindrome equal `gaga` be false', () => {
+    assert.equal(isPalindrome('gaga'), false);
+    expect(isPalindrome('gaga')).eq(false);
+  });
+  it('should function isPalindrome equal empty string', () => {
+    assert.equal(isPalindrome(''), true);
+    expect(isPalindrome('')).eq(true);
+  });
+});
 
 describe('function isOddNumber' , () => {
-    it('should function isOddNumber equal true', () => {
-        assert.equal(isOddNumber(7), true);
-        expect(isOddNumber(7)).eq(true);
-    })
-    it('should function isOddNumber equal false', () => {
-        assert.equal(isOddNumber(4), false);
-        expect(isOddNumber(4)).eq(false);
-    })
-    it('should function isOddNumber equal true', () => {
-        assert.equal(isOddNumber(-5), true);
-        expect(isOddNumber(-5)).eq(true);
-    })
-    it('should function isOddNumber equal false', () => {
-        assert.equal(isOddNumber(), false);
-        expect(isOddNumber()).eq(false);
-    })
-})
+  it('should function isOddNumber equal true', () => {
+    assert.equal(isOddNumber(7), true);
+    expect(isOddNumber(7)).eq(true);
+  });
+  it('should function isOddNumber equal false', () => {
+    assert.equal(isOddNumber(4), false);
+    expect(isOddNumber(4)).eq(false);
+  });
+  it('should function isOddNumber equal true', () => {
+    assert.equal(isOddNumber(-5), true);
+    expect(isOddNumber(-5)).eq(true);
+  });
+  it('should function isOddNumber equal false', () => {
+    assert.equal(isOddNumber(), false);
+    expect(isOddNumber()).eq(false);
+  });
+});
 
 describe('function isEvenNumber', () => {
-    it('should function isEvenNumber equal true', () => {
-        assert.equal(isEvenNumber(8), true);
-        expect(isEvenNumber(8)).eq(true);
-    })
-    it('should function isEvenNumber equal false', () => {
-        assert.equal(isEvenNumber(81), false);
-        expect(isEvenNumber(81)).eq(false);
-    })
-    it('should function isEvenNumber equal true', () => {
-        assert.equal(isEvenNumber(-18), true);
-        expect(isEvenNumber(-18)).eq(true);
-    })
-    it('should function isEvenNumber equal false', () => {
-        assert.equal(isEvenNumber(), false);
-        expect(isEvenNumber()).eq(false);
-    })
-})
+  it('should function isEvenNumber equal true', () => {
+    assert.equal(isEvenNumber(8), true);
+    expect(isEvenNumber(8)).eq(true);
+  });
+  it('should function isEvenNumber equal false', () => {
+    assert.equal(isEvenNumber(81), false);
+    expect(isEvenNumber(81)).eq(false);
+  });
+  it('should function isEvenNumber equal true', () => {
+    assert.equal(isEvenNumber(-18), true);
+    expect(isEvenNumber(-18)).eq(true);
+  });
+  it('should function isEvenNumber equal false', () => {
+    assert.equal(isEvenNumber(), false);
+    expect(isEvenNumber()).eq(false);
+  });
+});
 
 describe('function breakChocolate', () => {
-    it('should function breakChocolate equal 0', () => {
-        assert.equal(breakChocolate(-3, -5), 0);
-        expect(breakChocolate(7,-2)).eq(0);
-    })
-    it ('should function breakChocolate equal 9', () => {
-        assert.equal(breakChocolate(1, 10), 9);
-        expect(breakChocolate(1,10)).eq(9);
-    })
-    it('should function breakChocolate equal 23', () => {
-        assert.equal(breakChocolate(4, 6), 23);
-        expect(breakChocolate(4,6)).eq(23);
-    })
-    it('should function breakChocolate ecual null', () => {
-        assert.equal(breakChocolate(3, 'srtueivfj'), null);
-        expect(breakChocolate(2, )).eq(null);
-
-    })
-
-})
+  it('should function breakChocolate equal 0', () => {
+    assert.equal(breakChocolate(-3, -5), 0);
+    expect(breakChocolate(7,-2)).eq(0);
+  });
+  it ('should function breakChocolate equal 9', () => {
+    assert.equal(breakChocolate(1, 10), 9);
+    expect(breakChocolate(1,10)).eq(9);
+  });
+  it('should function breakChocolate equal 23', () => {
+    assert.equal(breakChocolate(4, 6), 23);
+    expect(breakChocolate(4,6)).eq(23);
+  });
+  it('should function breakChocolate equal null', () => {
+    assert.equal(breakChocolate(3, 'srtueivfj'), null);
+    expect(breakChocolate(2, )).eq(null);
+  });
+});
